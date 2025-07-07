dab7984194c32625a35b83ad9281288a55e8013f
LENOVO <LENOVO@LAPTOP-QVKCL1F9>
2025-07-07 21:47:51 +0800

    添加项目文件。

---
+++ test1.cpp
@@ -0,0 +1,18 @@
+﻿#include<string>
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+	vector<string> a;
+	a.push_back("hi");
+	a.push_back("hello");
+	a.push_back("world");
+	a.erase(a.begin() + 1);
+	a.insert(a.begin()+1, "hello");
+	for (int i = 0; i < a.size(); i++) {
+		cout << a[i] << endl;
+	}
+	return 0;
+}
+
